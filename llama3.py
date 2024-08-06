a7421fb9916d8ba825799551a2375de3bed9a715
Maddy Brown <brownm3@carleton.edu>
2024-08-06 14:40:46 -0500

    deleting old files and pushing finalized code

--- llama3.py
+++
@@ -1,6 +0,0 @@
-import ollama
-# Need to code all three stages with Llama
-my_model = "llama3"
-my_prompt = [{"role": "user", "content": "How should Amphetamine be used and what is the dosage?"}]
-response = ollama.chat(model = my_model, messages = my_prompt)
-print(response["message"]["content"])
